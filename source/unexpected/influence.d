fb2d3f03778e7436f48fdf3a0f9084e713c69cde
Cameron Ross <elpenguino@gmail.com>
2024-08-03 21:12:16 -0300

    support finite ranges without known min/max values for influencedChoice

--- source/unexpected/influence.d
+++ source/unexpected/influence.d
@@ -1,27 +1,35 @@
 /// Functions for "luck", or influenced results
 module unexpected.influence;
 
-import std.range : ElementType;
+import std.range : ElementType, isInfinite;
 
 enum hasMinMax(T) = is(typeof(T.min) == typeof(T.max)) && is(typeof(T.min) == T);
 
 auto influencedChoice(Range, Rand, Element = ElementType!Range)(double luck, ref Rand rng, Range range) if (hasMinMax!(ElementType!Range)) {
-	return influencedChoice(luck, rng, range, ElementType!Range.min, ElementType!Range.max);
+	return influencedChoiceCommon!true(luck, rng, range, ElementType!Range.min, ElementType!Range.max);
 }
-auto influencedChoice(Range, Rand, Element = ElementType!Range)(double luck, ref Rand rng, Range range, Element worst, Element best)
+auto influencedChoice(Range, Rand, Element = ElementType!Range)(double luck, ref Rand rng, Range range, Element worst, Element best) {
+	return influencedChoiceCommon!true(luck, rng, range, worst, best);
+}
+auto influencedChoice(Range, Rand, Element = ElementType!Range)(double luck, ref Rand rng, Range range) if (!isInfinite!Range && !hasMinMax!(ElementType!Range)) {
+	return influencedChoiceCommon!false(luck, rng, range, Element.init, Element.init);
+}
+auto influencedChoiceCommon(bool useWorstBest = true, Range, Rand, Element = ElementType!Range)(double luck, ref Rand rng, Range range, Element worst, Element best)
 	in(!range.empty, "Nothing to choose")
-	in(best >= worst, "Worst value must be less than best")
-	out(result; result >= worst, "Result is less than worst value")
-	out(result; result <= best, "Result is greater than best value")
+	in(!useWorstBest || (best >= worst), "Worst value must be less than best")
+	out(result; !useWorstBest || (result >= worst), "Result is less than worst value")
+	out(result; !useWorstBest || (result <= best), "Result is greater than best value")
 {
 	import std.algorithm.comparison : min;
 	import std.math : isInfinity;
 	import std.random : uniform01;
-	if (luck.isInfinity) {
-		if (luck > 0) {
-			return best;
-		} else {
-			return worst;
+	static if (useWorstBest) {
+		if (luck.isInfinity) {
+			if (luck > 0) {
+				return best;
+			} else {
+				return worst;
+			}
 		}
 	}
 	auto chosen = range.front;
@@ -31,10 +39,12 @@ auto influencedChoice(Range, Rand, Element = ElementType!Range)(double luck, ref
 		luck = -luck;
 	}
 	while ((luck > 0.0) && !range.empty) {
-		if (chosen > best) {
-			return best;
-		} else if (chosen < worst) {
-			return worst;
+		static if (useWorstBest) {
+			if (chosen > best) {
+				return best;
+			} else if (chosen < worst) {
+				return worst;
+			}
 		}
 		const remaining = min(luck, 1.0);
 		luck -= remaining;
@@ -93,13 +103,41 @@ auto influencedChoice(Range, Rand, Element = ElementType!Range)(double luck, ref
 	assert(test2(0.0, 0, 100) > test2(-1.0, 0, 100));
 }
 
+@safe pure unittest {
+	import std.random : Random, uniform;
+	import std.range : generate, take;
+	static struct Result {
+		int x;
+		int opCmp(Result b) const pure @safe {
+			return x - b.x;
+		}
+	}
+	Random rand;
+	enum iterations = 100;
+	double test(double luck, int min = int.min, int max = int.max) {
+		long total;
+		foreach (i; 0 .. iterations) {
+			const value = influencedChoice(luck, rand, generate!(() => Result(uniform(0, 100, rand))).take(100));
+			assert(value.x >= min);
+			assert(value.x <= max);
+			total += value.x;
+		}
+		return total / double(iterations);
+	}
+	assert(test(0.0) < test(1.0));
+	assert(test(0.0) > test(-1.0));
+	assert(test(double.infinity) > test(-double.infinity));
+	assert(test(double.infinity, 0, 100) > test(-double.infinity, 0, 100));
+	assert(test(0.0, 0, 100) > test(-1.0, 0, 100));
+}
+
 auto influencedWeightedChoice(Range, Weights, Rand, Element = ElementType!Range)(double luck, ref Rand rng, Range range, Weights weights) if (hasMinMax!(ElementType!Range)) {
 	return influencedWeightedChoice(luck, rng, range, weights, Element.min, Element.max);
 }
 auto influencedWeightedChoice(Range, Weights, Rand, Element = ElementType!Range)(double luck, ref Rand rng, Range range, Weights weights, Element worst, Element best) {
 	import std.random : dice;
 	import std.range : drop, front, generate;
-	return influencedChoice(luck, rng, generate!({ return range.drop(dice(rng, weights)).front; }), worst, best);
+	return influencedChoiceCommon!true(luck, rng, generate!({ return range.drop(dice(rng, weights)).front; }), worst, best);
 }
 
 @safe pure unittest {
@@ -139,7 +177,7 @@ auto influencedUniform(Rand, T)(double luck, ref Rand rng, T worst, T best) {
 		result.popFront();
 		return result;
 	}
-	return influencedChoice(luck, rng, uniformRange(rng, worst, best), worst, best);
+	return influencedChoiceCommon!true(luck, rng, uniformRange(rng, worst, best), worst, best);
 }
 
 @safe pure unittest {
